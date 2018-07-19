023a37bf709d84074f9a7f4f9aafcfa764fa18d3
ai-b <lexnrb@gmail.com>
2018-07-19 23:42:54 +0300

    Add files via upload

---
+++ MyForm.h
@@ -0,0 +1,156 @@
+﻿#pragma once
+
+namespace FactorialProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(77, 128);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(191, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = L"Please insert an integer number";
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Cambria Math", 14.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Underline)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(50, 19);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(242, 106);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"⋆Factorial Calculator⋆";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(65, 203);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(221, 16);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"The factorial of your number is:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Location = System::Drawing::Point(122, 237);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(102, 13);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"No number inserted!";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(125, 154);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(101, 35);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Calculate";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->ClientSize = System::Drawing::Size(332, 287);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+
+	String ^inputtext = textBox1->Text;
+	int inputinteger = System::Convert::ToInt16(inputtext);
+	int factorial = 1;
+	for (int i = 1; i <= inputinteger; i++)
+	{
+		factorial = factorial * i;
+	}
+	inputtext = System::Convert::ToString(factorial);
+	label3->Text = inputtext;
+}
+private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+}
+};
+}
